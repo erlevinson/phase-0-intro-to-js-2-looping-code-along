379b5d3aaf05ddc58928e97726af9fd8a9b741ea
Eric <ericlevinson3@gmail.com>
2022-08-06 12:14:25 -0500

    commit tree 48d97c0a3a576413b45798570482233149dcc554

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"]
+
+function wrapGifts(gifts){
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow`)
+        debugger
+    }
+    return gifts
+}
+
+
+function writeCards(array, string){
+    const newArray = []
+    for(let i = 0 ; i < array.length; i++){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(number){
+    while (number >= 0){
+    console.log(number--)
+    }
+}
